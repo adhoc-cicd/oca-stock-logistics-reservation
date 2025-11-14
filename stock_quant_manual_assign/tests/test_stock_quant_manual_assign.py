OCA/stock-logistics-warehouse

[MIG] stock_quant_manual_assign: Migration to 14.0

stock_quant_manual_assign/tests/test_stock_quant_manual_assign.py
@@ -132,7 +132,9 @@ class TestStockQuantManualAssign(TransactionCase):
         self.assertEqual(wizard.move_qty, 250.0)
         wizard.assign_quants()
         self.assertAlmostEqual(
-            len(self.move.move_line_ids), len(wizard.quants_lines.filtered("selected"))
+            len(self.move.move_line_ids),
+            2,
+            "Three are 2 quants selected",
         )
 
     def test_quant_assign_wizard_after_availability_check(self):

adhoc-cicd/oca-stock-logistics-reservation

stock_quant_manual_assign/tests/test_stock_quant_manual_assign.py
@@ -132,7 +132,9 @@ class TestStockQuantManualAssign(TransactionCase):
         self.assertEqual(wizard.move_qty, 250.0)
         wizard.assign_quants()
         self.assertAlmostEqual(
-            len(self.move.move_line_ids), len(wizard.quants_lines.filtered("selected"))
+            len(self.move.move_line_ids),
+            2,
+            "There are 2 quants selected",
         )
 
     def test_quant_assign_wizard_after_availability_check(self):